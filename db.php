a7fdba2b92984e8609a672f1c5482cc837b7d7a5
Artem1yMarenkov <orvysegor@gmail.com>
2021-03-26 11:55:00 +0300

    some fix in singup.php: code refactored

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $username = 'root';
 $password = '';
-$db = '';
+$db = 'daily_db';
 
 if (!$mysqli = new mysqli($host, $username, $password, $db))
     echo "Error db connection: ".$mysql->errno();
--- singup.php
+++ singup.php
@@ -1,17 +1,65 @@
-<?php 
+<?php
 
-function singup($db) : string 
+require 'db.php';
+
+function singup($db) : string
 {
     $login = trim(urldecode(htmlspecialchars($_POST['login'])));
     $password = md5(trim(urldecode(htmlspecialchars($_POST['password']))));
 
-    $sql = "INSERT INTO `users` (`login`, `password`) VALUES ($login, $password)";
 
-    if (!$db->query($sql)) {
-        return json_encode(array('registration'=> false));
+    if (strlen($login) > 11 || strlen($login) < 4) {
+      return json_encode(
+        array('registration' => false, 'error' => 'login lenght is incorrect')
+      );
+    }
+
+
+    if (strlen($password) > 50 || strlen($password) < 6 ) {
+      return json_encode(
+        array('registration' => false, 'error' => 'password lenght is incorrect')
+      );
+    }
+
+
+    if (!($sql = $db->prepare("INSERT INTO `users` (`login`, `password`) VALUES (?, ?)"))) {
+        return json_encode(
+          array('registration' => false, 'error' => $sql->error)
+        );
+    }
+
+
+    if (!($sql->bind_param("ss", $login, $password))) {
+        return json_encode(
+          array('registration' => false, 'error' => $sql->error)
+        );
+    }
+
+
+    if (!$sql->execute()) {
+
+        switch ($sql->error) {
+
+          case "Duplicate entry '$login' for key 'PRIMARY'":
+            return json_encode(
+              array('registration' => false, 'error' => 'login is not available')
+            );
+            break;
+
+          default:
+            return json_encode(
+              array('registration' => false, 'error' => $sql->error)
+            );
+            break;
+
+        }
     }
 
     unset($login, $password, $sql);
 
-    return json_encode(array('registration'=> true));
+    return json_encode(array('registration' => true, 'error' => false));
 }
+
+print_r(singup($mysqli));
+
+?>
